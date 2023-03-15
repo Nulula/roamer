5e11297e67b81a6d2e15dfc276560895c4230921
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-15 22:31:50 +0000

    moved functions and state from search form into wrapper

--- src/components/WrapperForSearch.js
+++ src/components/WrapperForSearch.js
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import GeoAPI from "../utils/GeoApi";
 import Map from "./Map";
 import Weather from "./Weather";
-import axios from "axios";
+import SearchForm from "./SearchForm";
 
 
 function WrapperForSearch() {
     // Setting states for search form input, search term for API call, and location info
-    const [input, setInput] = useState();
-    const [search, setSearch] = useState();
-    const [location, setLocation] = useState({
-        cityName: "",
-        countryCode: "",
+    const [formInputs, setFormInputs] = useState({
+        city: "",
+        country: ""
+    });
+    const [search, setSearch] = useState({
+        city: "",
+        country: ""
+    });
+    const [cityValue,setCityValue] = useState("");
+    const [countryValue,setCountryValue] = useState("");
+    const [coordinates, setCoordinates] = useState({
         lat: 0,
         lon: 0,
         key: ""
     });
 
+    useEffect(() => {
+        console.log(coordinates);
+    }, []);
+
     // API Call for location
-    const searchLocation = () => {
-        axios.get("https://api.geoapify.com/v1/geocode/search?city=Paris&country=FR&limit=1&format=json&apiKey=8f4690110c99450d8e8c77713b77c534")
+    const searchCoordinates = () => {
+        axios.get("https://api.geoapify.com/v1/geocode/search?city=" + search.city + "&country=" + search.country + "&limit=1&format=json&apiKey=8f4690110c99450d8e8c77713b77c534")
         .then((res) => {
             console.log(res);
-            setLocation({
-                ...location,
-                cityName: res.data.results[0].city,
+            setCoordinates({
                 lat: res.data.results[0].lat,
                 lon: res.data.results[0].lon,
                 key: res.data.results[0].place_id,
@@ -34,22 +43,41 @@ function WrapperForSearch() {
     }
 
     // Form functions
-    const handleInputChange = event => {setInput(event.target.value)}
+    // const handleInputChange = event => {setInput(event.target.value)}
+
+    const handleCityChange = (event) => {
+        setCityValue(event.target.value);
+    };
+
+    const handleCountryChange = (event) => {
+        setCountryValue(event.target.value);
+    };
 
     const handleSubmit = event => {
         event.preventDefault();
-        setSearch(input);
-        setInput("");
+        setFormInputs({
+            city: cityValue,
+            country: countryValue
+        });
+        setSearch({
+            city: formInputs.city,
+            country: formInputs.country
+        });
+        setCityValue("");
+        setCountryValue("");
     };
+
 
     return (
         <div>
-            <button onClick={searchLocation}>Click for API call</button>
-            {/* <SearchForm input={input}
-            handleSubmit={handleSubmit}
-            handleInputChange={handleInputChange}/>
+            <button onClick={searchCoordinates}>Click for API call</button>
+            <SearchForm cityValue={cityValue}
+            countryValue={countryValue}
+            handleCityChange={handleCityChange}
+            handleCountryChange={handleCountryChange}
+            handleSubmit={handleSubmit}/>
 
-            <Map cityName={location.cityName}
+            {/* <Map cityName={location.cityName}
             lat={location.lat}
             lon={location.lon}
             key={location.key} /> */}
