22c858a286c8c3aed069dadbf9f996d15046b65f
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-15 17:40:42 +0000

    import React, create empty function, and export

---
+++ src/components/WrapperForSearch.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+function WrapperForSearch() {
+    
+
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default WrapperForSearch;
