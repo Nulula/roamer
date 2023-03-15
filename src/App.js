ff98e67c1ac95065144e73f7d78a07e72b5ff56b
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-15 20:38:16 +0000

    basic css to set map size

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import Map from "./components/Map";
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
+        <Map />
         <Footer />
       </div>
     </Router>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,1 +1,21 @@
+import React from "react";
+import { MapContainer, TileLayer } from 'react-leaflet';
 
+function Map() {
+    
+    return (
+        <div>
+            <MapContainer 
+            center={[51.505, -0.09]} 
+            zoom={12}
+            scrollWheelZoom={false}>
+                <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </MapContainer>
+        </div>
+    )
+};
+
+export default Map;
