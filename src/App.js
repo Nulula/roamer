60331b8f97df63a902bb064562f6c3228f3261ec
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-15 20:11:44 +0000

    API call function added (static url for now)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import WrapperForSearch from "./components/WrapperForSearch";
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
+        <WrapperForSearch />
         <Footer />
       </div>
     </Router>
--- src/components/WrapperForSearch.js
+++ src/components/WrapperForSearch.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import GeoAPI from "../utils/GeoApi";
 import Map from "./Map";
 import Weather from "./Weather";
+import axios from "axios";
 
 
 function WrapperForSearch() {
@@ -10,13 +11,27 @@ function WrapperForSearch() {
     const [search, setSearch] = useState();
     const [location, setLocation] = useState({
         cityName: "",
+        countryCode: "",
         lat: 0,
         lon: 0,
         key: ""
     });
 
     // API Call for location
-    
+    const searchLocation = () => {
+        axios.get("https://api.geoapify.com/v1/geocode/search?city=Paris&country=FR&limit=1&format=json&apiKey=8f4690110c99450d8e8c77713b77c534")
+        .then((res) => {
+            console.log(res);
+            setLocation({
+                ...location,
+                cityName: res.data.results[0].city,
+                lat: res.data.results[0].lat,
+                lon: res.data.results[0].lon,
+                key: res.data.results[0].place_id,
+            });
+        })
+        .catch(err => console.log(err));
+    }
 
     // Form functions
     const handleInputChange = event => {setInput(event.target.value)}
@@ -29,14 +44,15 @@ function WrapperForSearch() {
 
     return (
         <div>
-            <SearchForm input={input}
+            <button onClick={searchLocation}>Click for API call</button>
+            {/* <SearchForm input={input}
             handleSubmit={handleSubmit}
             handleInputChange={handleInputChange}/>
 
             <Map cityName={location.cityName}
             lat={location.lat}
             lon={location.lon}
-            key={location.key} />
+            key={location.key} /> */}
         </div>
     )
 };
