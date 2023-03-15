9201cb7435c67f209c30ab484fff1a2b59eb01f0
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-15 18:45:16 +0000

    added states with useState

--- src/components/WrapperForSearch.js
+++ src/components/WrapperForSearch.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 function WrapperForSearch() {
-    
+    // Setting states for search form input, search term for API call, and location info
+    const [input, setInput] = useState();
+    const [search, setSearch] = useState();
+    const [location, setLocation] = useState();
 
     return (
         <div>
