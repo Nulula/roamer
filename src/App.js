7218c809a1489040d3ad2d18e3e116079469940a
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-03-16 11:13:19 +0000

    exported feedback component to contact page and linked to nav bar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Feedback from "./components/Feedback";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
-        <Feedback />
+        <Routes>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
         <Footer />
       </div>
     </Router>
