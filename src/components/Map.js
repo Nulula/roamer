73c74112e93144bd3ac8421a1772fefe5fb8817a
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-16 18:51:55 +0000

    the map will display London as a placeholder until it gets coordinates

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { MapContainer, TileLayer } from 'react-leaflet';
 
-function Map() {
+function Map({lat, lon}) {
+
     
     return (
         <div>
             <MapContainer 
-            center={[51.505, -0.09]} 
+            center={lat && lon != 0 ? [lat, lon] : [51.509865, -0.118092]} 
             zoom={12}
             scrollWheelZoom={false}>
                 <TileLayer
