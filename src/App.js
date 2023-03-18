52afed261a6c28d5692072744d1c9fec36a06e16
DessySI <divanova.docs.manager@gmail.com>
2023-03-18 16:50:00 +0000

    modal added, home page added, logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // This code imports necessary components and sets up routing for the app
-import React from "react"; // Importing the React library
+import React, { useState } from "react"; // Importing the React library
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing routing components from react-router-dom
 import Nav from "./components/Nav"; // Importing the Navigation component
 import Footer from "./components/Footer"; // Importing the Footer component
 import Contact from "./pages/Contact"; // Importing the Contact page component
-import Profile from "./pages/Profile"; // Importing the Profile page component
+import Profile from "./pages/Profile";
 import WrapperForSearch from "./components/WrapperForSearch"; // Importing the WrapperForSearch component
-import Login from "./pages/Login"; // Importing the Login page component
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import HomePage from "./pages/Home";
 
 function App() {
   return (
@@ -14,9 +17,10 @@ function App() {
       <div>
         <Nav /> {/* Rendering the Navigation component*/}
         <Routes>
-          <Route path="/" element={<WrapperForSearch />}></Route>{" "}
+          <Route path="/" element={<HomePage />}>
+            {" "}
+          </Route>{" "}
           {/*Defining */}
-          the route for the WrapperForSearch component
           <Route path="/contact" element={<Contact />}></Route>{" "}
           {/*Defining the
           route for the Contact page component*/}
@@ -26,11 +30,12 @@ function App() {
           <Route path="/nearme" element={<WrapperForSearch />}></Route>{" "}
           {/*
           Defining the route for the WrapperForSearch component*/}
-          <Route path="/login" element={<Login />}></Route>{" "}
+          <Route path="/home" element={<HomePage />}></Route>{" "}
           {/* Defining the
           route for the Login page component*/}
         </Routes>
         <Footer /> {/* Rendering the Footer component*/}
+        <ToastContainer />
       </div>
     </Router>
   );
