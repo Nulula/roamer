cc54086a70715d3a64e2b2d1ff11ff77148b9480
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-03-20 11:25:10 +0000

    ul list changed to ol to number the answers

--- src/components/PlacesInfo.js
+++ src/components/PlacesInfo.js
@@ -13,20 +13,17 @@ function PlacesInfo({ data }) {
   }, [data]);
 
   return (
-    <div>
-      {/* <h1>Your search reasults:</h1> */}
-      <div className="placesList">
-        <ul>
-          {places.map((place) => {
-            return (
-              <li key={place.properties.datasource.raw.osm_id}>
-                <h2>{place.properties.address_line1}</h2>
-                <p>{place.properties.address_line2}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+    <div className="placesList">
+      <ol>
+        {places.map((place) => {
+          return (
+            <li key={place.properties.datasource.raw.osm_id}>
+              <h2>{place.properties.address_line1}</h2>
+              <p>{place.properties.address_line2}</p>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
