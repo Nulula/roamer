d181ee9aec5065ef9494b1595225d0ef8078308f
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-03-21 20:24:59 +0000

    pages all have same style code for background img

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Feedback from "../components/Feedback";
+import bgImage from "../assets/backgroundImg.jpg";
 
 function Contact() {
+  const styles = {
+    backgroundImage: `url(${bgImage})`,
+    backgroundSize: "cover",
+    height: "88vh",
+  };
+
   return (
-    <div className="feedback">
-      <Feedback />
+    <div style={styles}>
+      <div className="feedback">
+        <Feedback />
+      </div>
     </div>
   );
 }
